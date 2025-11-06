c20f7a1806cf6b8658e126fa4d85f488303e846c
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-07 00:17:41 +0100

    task-1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+
+}
+
+const teacher1: Teacher = {
+firstName: 'grace',
+lastName: 'ebe',
+fullTimeEmployee: true,
+yearsOfExperience: 1,
+location: 'lagos',
+contract: false
+
+}
+
+console.log(teacher1)
