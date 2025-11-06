4fbedfa39f365498a448df7319fcce5cf4ecea93
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-06 14:03:27 +0100

    update

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,15 +20,21 @@ const student2: Student = {
 const studentList: Student[] = [student1, student2];
 
 const table = document.createElement('table')
-const headerRow = table.insertRow();
-const header1 = headerRow.insertCell();
-const header2 = headerRow.insertCell();
-
+const thead = table.createTHead();
+const headerRow = thead.insertRow();
+const header1 = document.createElement('th')
 header1.textContent = 'First Name';
+const header2 = document.createElement('th');
 header2.textContent = 'Location';
 
+
+headerRow.appendChild(header1)
+headerRow.appendChild(header2)
+
+const tbody = table.createTBody();
+
 studentList.forEach((student) => {
-    const row = table.insertRow();
+    const row = tbody.insertRow();
     const firstNameCell = row.insertCell();
     const locationCell = row.insertCell();
 
