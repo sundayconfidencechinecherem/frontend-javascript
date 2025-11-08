dbf759d484c171a5fa64bbfe3aeb55606e37d9a0
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-08 08:29:43 +0100

    fix

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -2,31 +2,26 @@
 /// <reference path="subjects/Java.ts" />
 /// <reference path="subjects/React.ts" />
 
-// Create and export constants for each subject
 export const cpp = new Subjects.Cpp();
 export const java = new Subjects.Java();
 export const react = new Subjects.React();
 
-// Create and export one Teacher object with experienceTeachingC = 10
 export const cTeacher: Subjects.Teacher = {
-  firstName: "John",
-  lastName: "Doe",
-  experienceTeachingC: 10
+    firstName: "John",
+    lastName: "Doe",
+    experienceTeachingC: 10
 };
 
-// For Cpp subject
 console.log('C++');
 cpp.setTeacher(cTeacher);
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
-// For Java subject
 console.log('Java');
 java.setTeacher(cTeacher);
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
-// For React subject
 console.log('React');
 react.setTeacher(cTeacher);
 console.log(react.getRequirements());
