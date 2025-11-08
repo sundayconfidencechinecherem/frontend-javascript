b1ba8113b604fabbac5da8e7e3f0d1759ffc2349
sundayconfidencechinecherem <sundayconfidencechinecherem@gmail.com>
2025-11-08 08:14:33 +0100

    fix

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,13 +1,9 @@
 namespace Subjects {
   export class Subject {
-    private _teacher: Teacher;
+    teacher: Teacher;
 
-    setTeacher(teacher: Teacher): void {
-      this._teacher = teacher;
-    }
-
-    get teacher(): Teacher {
-      return this._teacher;
+    setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
     }
   }
 }
